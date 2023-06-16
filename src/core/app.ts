6608366e1c7fc8e5be5ced87a79fa7748bb3f9bb
fweise <public@weise-games.de>
2023-06-16 17:24:58 +0200

    update frameworks

--- src/core/app.ts
+++ src/core/app.ts
@@ -3,7 +3,7 @@ import * as CommanderJS from "commanderjs";
 import * as ModuleJS from "modulejs";
 import { Server, ServerConfig } from "./server";
 
-interface Config {
+export interface Config {
     readonly debug?: boolean;
     readonly name?: string;
     readonly version?: string;
@@ -12,27 +12,27 @@ interface Config {
     readonly modules?: ReadonlyArray<CoreJS.LoadModuleConfig & { readonly config?: any; }>;
 }
 
+export interface Options {
+    readonly globalParams?: readonly CommanderJS.Parameter<any>[];
+    readonly globalArgs?: NodeJS.ReadOnlyDict<any>;
+}
+
 export class App {
     public readonly onMessage = new CoreJS.Event<App, string>('App.onMessage');
     public readonly onError = new CoreJS.Event<App, Error>('App.onError');
 
     public readonly config: Config;
 
-    private readonly modules: readonly ModuleJS.Module<any>[];
+    private readonly modules: readonly ModuleJS.Module<any, any, any>[];
 
     private readonly commander: CommanderJS.Commander;
     private readonly server: Server;
 
-    constructor(
-        config: Config & ServerConfig,
-        globalArgs: NodeJS.ReadOnlyDict<any> = {},
-        globalParams: readonly CommanderJS.Parameter<any>[] = []
-    ) {
-        globalParams = Object.assign([
-            new CommanderJS.BoolParameter('debug', 'enables/disables debug mode', false)
-        ], globalParams);
+    private isCLI = false;
 
-        globalArgs = CommanderJS.parseArgs(globalArgs, globalParams);
+    constructor(config: Config & ServerConfig, options: Options = {}) {
+        const globalParams = Object.assign([], options.globalParams || [], ModuleJS.Parameters);
+        const globalArgs = CommanderJS.parseArgs(options.globalArgs, globalParams);
 
         const infos: any = CoreJS.loadConfig('package.json');
         const debug = config.debug || globalArgs.debug;
@@ -51,7 +51,7 @@ export class App {
             description
         };
 
-        this.modules = (config.modules || []).map(data => CoreJS.loadModule(data, data.config));
+        this.modules = (config.modules || []).map(data => CoreJS.loadModule(data, data.config, globalArgs));
         this.modules.forEach(module => module.onMessage.on(message => this.onMessage.emit(this, message)));
 
         this.commander = new CommanderJS.Commander({
@@ -77,32 +77,28 @@ export class App {
     public get name(): string { return this.config.name; }
     public get debug(): boolean { return this.config.debug; }
 
-    public async init(admin: boolean) {
-        const options: ModuleJS.Options = {
-            debug: this.debug,
-            admin
-        };
-
+    public async init(flags: readonly string[] = [ModuleJS.Flag.CLI]) {
+        this.isCLI = flags.includes(ModuleJS.Flag.CLI);
         this.commander.clear();
 
-        if (admin) {
+        if (this.isCLI) {
             this.commander.set({
                 name: 'help',
-                action: async args => new CoreJS.TextResponse(this.commander.help(args.command && args.command.toString())),
                 description: 'Lists all commands or returns details of specific <command>.',
                 parameters: [
                     new CommanderJS.StringParameter('command', 'Lists all commands with this prefix or returns details of specific command.', '')
-                ]
+                ],
+                execute: async args => new CoreJS.TextResponse(this.help(args.command && args.command.toString()))
             });
 
             this.commander.set({
                 name: 'start',
                 description: "starts the server",
-                action: async () => {
+                execute: async () => {
                     if (this.server.isRunning)
                         return new CoreJS.ErrorResponse(CoreJS.ResponseCode.Forbidden, 'server is running already');
 
-                    await this.init(false);
+                    await this.init(flags.filter(flag => flag != ModuleJS.Flag.CLI));
                     await this.server.start();
 
                     return new CoreJS.TextResponse("server stopped");
@@ -115,7 +111,7 @@ export class App {
                 parameters: [
                     new CommanderJS.ArrayParameter('module', 'to revert', null)
                 ],
-                action: async args => {
+                execute: async args => {
                     const modules = args.module
                         ? this.modules.filter(module => args.module.includes(module.name))
                         : this.modules;
@@ -132,7 +128,7 @@ export class App {
                 parameters: [
                     new CommanderJS.ArrayParameter('module', 'to revert', null)
                 ],
-                action: async args => {
+                execute: async args => {
                     const modules = args.module
                         ? this.modules.filter(module => args.module.includes(module.name))
                         : this.modules;
@@ -150,7 +146,7 @@ export class App {
                     new CommanderJS.ArrayParameter('module', 'to revert', null),
                     new CommanderJS.NumberParameter('version', 'all versions above or equal will be reverted', 0)
                 ],
-                action: async args => {
+                execute: async args => {
                     const modules = args.module
                         ? this.modules.filter(module => args.module.includes(module.name))
                         : this.modules;
@@ -162,7 +158,12 @@ export class App {
             });
         }
 
-        await Promise.all(this.modules.map(async module => module.init(options).then(commands => commands.forEach(command => this.commander.set(command)))));
+        await Promise.all(this.modules.map(async module => module.init(flags).then(commands => commands.forEach(command => {
+            command.onMessage.off({ listener: this.onMessage });
+            command.onMessage.on((message, command) => this.onMessage.emit(this, `${command.name}: ${message}`), { listener: this.onMessage });
+
+            this.commander.set(command);
+        }))));
     }
 
     public async close(): Promise<void> {
@@ -175,13 +176,17 @@ export class App {
             if (!command)
                 return new CoreJS.TextResponse(this.help(args.command && args.command.toString()));
 
-            const validationResponse = (await Promise.all(this.modules.map(module => module.validate(command, args))))
+            let errorResponse = (await Promise.all(this.modules.map(module => module.prepare(command, args))))
                 .find(result => result);
 
-            if (validationResponse)
-                return validationResponse;
+            if (errorResponse)
+                return errorResponse;
 
-            return await this.commander.execute(command, args);
+            const result = await this.commander.execute(command, args);
+
+            await Promise.all(this.modules.map(module => module.finish(command, args)));
+
+            return result;
         } catch (error) {
             const code = isNaN(error.code)
                 ? CoreJS.ResponseCode.InternalServerError
@@ -210,7 +215,9 @@ export class App {
     }
 
     public help(prefix?: string) {
-        let result = this.commander.help(prefix);
+        let result = this.commander.help(prefix, {
+            globalParameters: this.isCLI
+        });
 
         if (this.modules.length)
             result += '\nModules:\n' + this.modules.map(modules => modules.name).join('\n') + '\n';
