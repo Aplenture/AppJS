5d4909a808bc5e6972a6267dc2ac55201049cc42
fweise <public@weise-games.de>
2023-09-07 15:38:59 +0200

    fix runtime handling

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ import * as HTTP from "http";
 import { App, Server } from "./core";
 
 const PARAMETER_LOGFILE = 'logfile';
+const PATH_CONFIG = 'configs/';
+const DEFAULT_CONFIG = 'config.json';
 
 const command = process.argv[2];
 const route = process.argv[3] && 0 != process.argv[3].indexOf('-')
@@ -24,18 +26,23 @@ const commander = new CoreJS.Commander();
 const config = new CoreJS.Config(...App.Parameters, ...Server.Parameters, ...BackendJS.Module.GlobalParameters);
 const infos = BackendJS.loadConfig('package.json');
 
+let server: Server;
+
 config.add(new CoreJS.StringParameter(PARAMETER_LOGFILE, 'file path of log file', './app.log'));
 config.set(App.PARAMETER_VERSION, infos.version);
-config.deserialize(BackendJS.loadConfig('configs/config.json'));
+config.deserialize(BackendJS.loadConfig(PATH_CONFIG + DEFAULT_CONFIG));
 config.deserialize(globalArgs);
 
 commander.set({
     name: 'start',
     description: "starts the server",
     parameters: new CoreJS.ParameterList(
-        new CoreJS.StringParameter('config', 'filepath of additional config', 'server')
+        new CoreJS.StringParameter('config', 'filepath of additional config', 'server.json')
     ),
     execute: async args => {
+        if (server)
+            return "server is already running\n";
+
         const additionalConfigPath: string = args.config;
 
         delete args.config;
@@ -46,26 +53,26 @@ commander.set({
 
         const log = BackendJS.Log.Log.createFileLog(config.get(PARAMETER_LOGFILE));
 
-        process.on('exit', code => code && log.write("exit with code " + code));
-        process.on('SIGINT', () => log.close().then(() => process.exit()));
-        process.on('SIGUSR1', () => log.close().then(() => process.exit()));
-        process.on('SIGUSR2', () => log.close().then(() => process.exit()));
+        log.write(`loaded config '${DEFAULT_CONFIG}'`);
+        if (additionalConfigPath) log.write(`loaded config '${additionalConfigPath}'`);
+
+        process.on('exit', code => process.stdout.write(`server stopped${code ? ` with exit code ${code}` : ''}\n`));
+        process.on('SIGINT', () => commander.execute('stop'));
+        process.on('SIGUSR1', () => commander.execute('stop'));
+        process.on('SIGUSR2', () => commander.execute('stop'));
         process.on('uncaughtException', error => log.error(error));
         process.on('unhandledRejection', reason => log.error(reason instanceof Error ? reason : reason ? new Error(reason.toString()) : new Error()));
 
         const app = new App(config, args);
-        const server = new Server(app, config);
+        app.onMessage.on((message, sender) => log.write(message, sender.name + ' (App)'));
+        app.onError.on((error, sender) => log.error(error, sender.name + ' (App)'));
+        await app.init();
 
         process.title = app.name;
 
-        app.onMessage.on(message => log.write(message));
-        app.onError.on(error => log.error(error));
-
-        server.onMessage.on(message => log.write(message));
-        server.onError.on(error => log.error(error));
-
-        await app.init();
-
+        server = new Server(app, config);
+        server.onMessage.on((message, sender) => log.write(message, sender.app.name + ' (Server)'));
+        server.onError.on((error, sender) => log.error(error, sender.app.name + ' (Server)'));
         server
             .start()
             .then(() => app.deinit())
@@ -76,10 +83,23 @@ commander.set({
 });
 
 commander.set({
+    name: 'stop',
+    description: "stops the server",
+    execute: async args => {
+        if (!server)
+            return "server is not running\n";
+
+        server.stop();
+
+        return "server stopped\n";
+    }
+});
+
+commander.set({
     name: 'exec',
     description: 'sends request to a server instance',
     parameters: new CoreJS.ParameterList(
-        new CoreJS.StringParameter('config', 'filepath of additional config', 'local'),
+        new CoreJS.StringParameter('config', 'filepath of additional config', 'local.json'),
         new CoreJS.BoolParameter('start', 'starts server if it is not running already', true),
     ),
     execute: async args => {
@@ -129,16 +149,17 @@ commander.set({
                         if (!start)
                             return resolve('server is not running\n');
 
-                        // starts the server
+                        // start the server
                         // and retry exec
                         return commander
-                            .execute('start', { config: '' })
+                            .execute('start', { config: additionalConfigPath })
                             .then(() => commander.execute('exec', Object.assign({}, args, {
                                 config: '',
                                 start: false
                             })))
                             .then(resolve)
-                            .catch(reject);
+                            .catch(reject)
+                            .then(() => commander.execute('stop'));
 
                     default:
                         return reject(error);
@@ -166,7 +187,7 @@ commander.set({
         new CoreJS.StringParameter('path', 'config file path')
     ),
     execute: async args => {
-        const path = `configs/${args.path}.json`;
+        const path = `${PATH_CONFIG}${args.path}`;
 
         if (!FS.existsSync(path))
             return `not existing config file at '${path}'`;
@@ -183,12 +204,12 @@ commander.set({
     description: "writes config to file",
     parameters: new CoreJS.ParameterList(
         new CoreJS.NumberParameter('space', 'serialization option space', 4),
-        new CoreJS.ArrayParameter('path', 'config file path', new CoreJS.StringParameter('', ''), ['config']),
+        new CoreJS.ArrayParameter('path', 'config file path', new CoreJS.StringParameter('', ''), ['config.json']),
         new CoreJS.BoolParameter('force', 'overwrites existing config file', false)
     ),
     execute: async args => {
         const result = await Promise.all(args.path.map(async path => {
-            path = `configs/${path}.json`;
+            path = `${PATH_CONFIG}${path}`;
 
             if (FS.existsSync(path) && !args.force)
                 return `config file already exist at '${path}'`;
@@ -215,5 +236,4 @@ commander.set({
 commander
     .execute(command, globalArgs)
     .then(result => process.stdout.write(result))
-    .catch(error => process.stdout.write(error.stack))
-    .then(() => process.exit()); 
+    .catch(error => process.stdout.write(error.stack)); 
